cb50bdd24ad8c55f716711498300ef29aee548b4
Clement Pigeron <clement.pigeron@gmail.com>
2025-04-07 13:32:56 +0200

    ecriture operator

--- script_cc.js
+++ script_cc.js
@@ -1,3 +1,13 @@
+/*let positionSelector = 1
+
+document.getElementById("top").addEventListener("click", () => {
+    positionSelector = 1
+})
+document.getElementById("bottom").addEventListener("click", () => {
+    positionSelector = 2
+})
+*/
+
 let digit = document.querySelectorAll(".digit")
 digit.forEach(
     (btn) => {
@@ -7,6 +17,25 @@ digit.forEach(
     }
 )
 
+let operator = document.querySelectorAll(".operator")
+operator.forEach(
+    (btn) => {
+        btn.addEventListener("click", () => {
+            writeOperator(btn.textContent)
+        })
+    }
+)
+
 function writeDigit (nb) {
-    document.getElementById("monitor").innerHTML = `${nb}`
+    /*if (positionSelector===1) {
+        document.getElementById("baseLine").innerHTML += `${nb}`
+    } else {
+        document.getElementById("otherLine").innerHTML += `${nb}`
+    }*/
+
+    document.getElementById("baseLine").innerHTML += `${nb}`
+}
+
+function writeOperator (op) {
+    document.getElementById("operatorLine").innerHTML = `${op}`
 }
